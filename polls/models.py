0dc3e762063fdb430d76b008e82b515da73fa995
James Stuckey <jstuckey@rutgers.edu>
2018-10-28 22:18:34 -0400

    made migrations for choice and question

--- polls/models.py
+++ polls/models.py
@@ -12,6 +12,6 @@ class Question(models.Model):
 
 
 class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.cascade)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
