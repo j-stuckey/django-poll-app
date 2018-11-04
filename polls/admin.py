9aab02ed808f7e9d48343086ec3ca10992f0179a
James Stuckey <jstuckey@rutgers.edu>
2018-11-03 22:21:04 -0400

    finished part two of tutorial

--- polls/admin.py
+++ polls/admin.py
@@ -3,4 +3,10 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
+from .models import Question
+
 # Register your models here.
+
+# shows the Questions from the Polls App on the admin page
+
+admin.site.register(Question)
